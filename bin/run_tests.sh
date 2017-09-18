f9ed3ae7a663b2ccf40aa192d6a849b4cd833148
Riccardo Fellegara <felle@umd.edu>
2017-09-18 17:59:03 -0400

    added utilities function from upstream.. synched module updated test script

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd ..
-make test_index_generation test_soup test_curvature test_morse test_morse_simplification 
-make test_batched_queries test_spatial_queries test_terrain_feature_extraction test_multivariate_morse
+make tests
 cd bin/
 
 echo "===[TEST 1]=== INDEX GENERATION === "
@@ -11,18 +10,11 @@ echo "===[TEST 2]=== FROM SOUP TO INDEXED MESH ==="
 ./test_soup
 echo "===[TEST 3]=== CURVATURE ==="
 ./test_curvature
-echo "===[TEST 4]=== MORSE TERRAIN FEATURE EXTRACTION"
-./test_morse
-echo "===[TEST 5]=== MORSE TERRAIN FEATURE SIMPLIFICATION ==="
-./test_morse_simplification
-echo "===[TEST 6]=== BATCHED TOPOLOGICAL QUERIES ==="
+echo "===[TEST 4]=== BATCHED TOPOLOGICAL QUERIES ==="
 ./test_batched_queries
-echo "===[TEST 7]=== SPATIAL QUERIES ==="
+echo "===[TEST 5]=== SPATIAL QUERIES ==="
 ./test_spatial_queries
-echo "===[TEST 8]=== TERRAIN FEATURE EXTRACTION"
+echo "===[TEST 6]=== TERRAIN FEATURE EXTRACTION"
 ./test_terrain_feature_extraction
-echo "===[TEST 9]=== MULTIVARIATE MORSE TERRAIN FEATURE EXTRACTION"
-./test_multivariate_morse
 
-rm test_index_generation test_soup test_curvature test_morse test_morse_simplification 
-rm test_batched_queries test_spatial_queries test_terrain_feature_extraction test_multivariate_morse
+rm test_index_generation test_soup test_curvature test_batched_queries test_spatial_queries test_terrain_feature_extraction 
---
+++ sources/utilities/utility_functions.h
@@ -0,0 +1,72 @@
+/*
+    This file is part of the Terrain Trees library.
+
+    Author(s): Riccardo Fellegara (riccardo.fellegara@gmail.com)
+
+    This project has been supported by the Italian Ministry of Education and
+    Research under the PRIN 2009 program, and by the National Science Foundation
+    under grant number IIS-1116747.
+
+    The Terrain Trees library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Terrain Trees library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Terrain Trees library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef MAIN_UTILITY_FUNCTIONS_H
+#define MAIN_UTILITY_FUNCTIONS_H
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+#include <sstream>
+
+#include "geometry/geometry_wrapper.h"
+#include "io/reader.h"
+#include "io/writer.h"
+#include "queries/spatial_queries.h"
+#include "queries/topological_queries.h"
+#include "statistics/statistics.h"
+#include "terrain_trees/spatial_subdivision.h"
+#include "terrain_trees/prt_tree.h"
+#include "terrain_trees/pmt_tree.h"
+#include "terrain_trees/pmrt_tree.h"
+#include "terrain_trees/reindexer.h"
+#include "utilities/input_generator.h"
+#include "utilities/string_management.h"
+#include "utilities/timer.h"
+#include "curvature/concentrated_curvature.h"
+#include "curvature/c_curvature.h"
+#include "terrain_features/slope_extractor.h"
+#include "terrain_features/critical_points_extractor.h"
+
+#include "utilities/cli_parameters.h"
+#include "utilities/usage.h"
+
+using namespace std;
+using namespace string_management;
+
+namespace utility_functions
+{
+extern string SpatialDecType2string(SpatialDecType sdt);
+extern int read_arguments(int argc, char** argv, cli_parameters &variables);
+extern void print_usage();
+extern void setParameters(cli_parameters &variables);
+extern bool checkParameters(cli_parameters &variables);
+extern void print_help();
+extern void print_paragraph(string stringa, int cols);
+}
+
+#endif // MAIN_UTILITY_FUNCTIONS_H
